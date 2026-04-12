35c6ff48f63ea92638dfe0298c55ad2c00d88d3d
Prabhat <osrprabhatyadav4@gmail.com>
2026-04-13 00:24:09 +0530

    feat: connect to database

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+/*
+
+import express from "express";
+const app = express();
+
+;(async () => {
+  try {
+    await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`);
+    app.on("ERROR", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR: ", error); 
+    throw error;
+  }
+})();
+
+*/
