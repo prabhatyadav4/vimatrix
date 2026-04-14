6c0860d4dc9e7e9508053ba2ae5e2cb75b44fd14
Prabhat <osrprabhatyadav4@gmail.com>
2026-04-15 00:25:47 +0530

    feat: custom api response and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,53 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connected successfully", error);
+  });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 /*
 
 import express from "express";
