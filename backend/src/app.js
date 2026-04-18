4daf2eab3583f57bc422e0fc6c9267203331e74e
Prabhat <osrprabhatyadav4@gmail.com>
2026-04-19 01:25:14 +0530

    feat: register user route created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running 🚀");
+// });
 
 app.use(
   cors({
@@ -10,8 +15,13 @@ app.use(
   })
 );
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-export { app };
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+export default app;
